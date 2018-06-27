5972fd24b18d6be167f73776d46642632c5a363c
cktang88 <arctangent20@yahoo.com>
2018-06-26 23:07:09 -0400

    Move to correct config dir

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ Config.py holds all config info
 
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
-port=5002
-host='0.0.0.0'
+UPLOAD_FOLDER = '/uploads'
+# unused
+# PORT = 5000
+# HOST = '0.0.0.0'
