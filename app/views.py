bcdfca40824771de47167d02ef967d3391a00a81
cktang88 <arctangent20@yahoo.com>
2018-06-26 23:07:48 -0400

    Add file upload

--- app/views.py
+++ app/views.py
@@ -3,10 +3,11 @@ Views.py holds all views of the app
 '''
 
 
-from flask import Flask, request, render_template, redirect, url_for, flash
-from werkzeug import import_string, cached_property
+from flask import Flask, request, render_template, redirect, url_for, flash, send_from_directory
+from werkzeug.utils import secure_filename
 
 from app import app
+import os
 
 # NOTE: Flask defaults to serving html out of /templates, statics (js/css) out of /static
 
@@ -14,3 +15,32 @@ from app import app
 def index():
     if request.method == 'GET':
         return render_template("index.html")
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'pdf'
+
+'''
+Serves csv file to front-end
+'''
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route('/api', methods=['POST'])
+def api():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        # http://werkzeug.pocoo.org/docs/0.14/utils/#werkzeug.utils.secure_filename
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename=filename))
