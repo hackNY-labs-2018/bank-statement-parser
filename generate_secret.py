4c48015bfde45d9cc8355f3d8fe13d8410a26c4f
Martha Edwards <martha_edwards@brown.edu>
2018-06-28 21:53:25 -0400

    Freeze requirements and update secret key instructions

--- generate_secret.py
+++ generate_secret.py
@@ -7,6 +7,7 @@ From https://gist.github.com/geoffalday/2021517
 
 import os, binascii
 print('\nGenerating a secret key...')
-print('Copy and paste the following in `instance/config.py` as the SECRET entry:')
+print('Create or edit the file `instance/config.py`, to include the line')
+print('SECRET = `xxx` replacing `xxx` with the below:')
 print(binascii.hexlify(os.urandom(24)))
 print('')
